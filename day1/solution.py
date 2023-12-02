ded58e484bdfe09913f25f22587918b59bf49d90
Tim Harrold <timjhh@amazon.com>
2023-12-02 01:58:53 +0000

    day 1 solution

--- day1/solution.py
+++ day1/solution.py
@@ -1,35 +1,14 @@
 import os, re
+# 53592
+# d = {"one": "1","two": "2","three": "3","four": "4","five": "5","six": "6","seven": "7","eight": "8","nine": "9","zero": "0",}
+# sum = 0
+# def find_first(st):
+#     return min([(st.find(e),d[e] if e in d else e) for e in (list(d.keys())+list(d.values())) if e in st])[1]
+# def find_last(st):
+#     return max([(st.rfind(e),d[e] if e in d else e) for e in (list(d.keys())+list(d.values())) if e in st])[1]
+# for l in open("input.txt", "r"):
+    #sum = sum + int(find_first(l)+find_last(l))
+# print(sum)
 
-f = open("i2.txt", "r")
-
-d = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
-    "zero": 0,
-}
-
-sum = 0
-for l in f:
-    if len(l) == 0:
-        continue
-    #ne = " ".join(d.get(e, e) for e in l.split())
-    pat = '|'.join(sorted(re.escape(k) for k in l))
-    ne = re.sub(pat, lambda m: l.get(m.group(0).upper()))
-    le = re.sub(r'\D', '', l)
-    print("OLD 1:", l, " OLD: ", le, " NEW: ", ne)
-    if len(le) < 2:
-        continue
-    elif len(le) == 2:
-        sum = sum + int(le)
-    else:
-        print("NUM: ", le, " NEW: ", int(le[0] + le[-1]))
-        sum = sum + int(le[0] + le[-1])
-
-print(sum)
+d = {"one": "1","two": "2","three": "3","four": "4","five": "5","six": "6","seven": "7","eight": "8","nine": "9","zero": "0",}
+print(sum([int(min([(st.find(e),d[e] if e in d else e) for e in (list(d.keys())+list(d.values())) if e in st])[1] + max([(st.rfind(e),d[e] if e in d else e) for e in (list(d.keys())+list(d.values())) if e in st])[1]) for st in open("input.txt", "r")]))
